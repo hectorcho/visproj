ed3fcf8a84c723cf765ff66f1db145310469e09e
xharlie <charlie.xu007@yahoo.com>
2017-10-23 02:06:21 -0400

    sync eeg graph with eeg scroll bar and add eeg file upload at menu bar

--- real_time.py
+++ real_time.py
@@ -7,7 +7,7 @@ import pyqtgraph.examples
 import math
 # pyqtgraph.examples.run()
 # comment
-
+import user
 from vlc_widget import VLCPlayerWidget
 from ui_menubar import Ui_Menubar
 
@@ -29,6 +29,8 @@ except AttributeError:
 class Ui_MainWindow(object):
     def __init__(self):
         self.time = 0
+        self.eeg = None
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(800, 600)
@@ -48,13 +50,14 @@ class Ui_MainWindow(object):
         #self.slider.setGeometry(QtCore.QRect(20,500,500,60))
 
         self.graphicsView = pg.PlotWidget(self.centralWidget)
-        self.graphicsView.setGeometry(QtCore.QRect(20, 750, 1280, 270))
+        self.graphicsView.setGeometry(QtCore.QRect(20, 750, 1280, 170))
         self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
 
 
         self.eegScroll = QtGui.QScrollBar(QtCore.Qt.Horizontal, MainWindow)
-        self.eegScroll.setGeometry(QtCore.QRect(20, 1050, 1280, 10))
+        self.eegScroll.setGeometry(QtCore.QRect(20, 950, 1280, 10))
         self.eegScroll.setMaximum(1000)
+        self.eegScroll.sliderMoved.connect(self.update_eeg_graph)
 
         self.vlcslider = self.vlcplayer.positionSlider
         self.vlctimer = self.vlcplayer.timer
@@ -71,11 +74,10 @@ class Ui_MainWindow(object):
         self.menuBar.setGeometry(QtCore.QRect(0, 0, 800, 22))
         self.menuBar.setObjectName(_fromUtf8("menuBar"))
 
-        #self.eeg_menu = self.menuBar.addMenu('&File')
-
-
-        #self.load_file = QtGui.QAction("&Load File", self)
-        #self.load_file.triggered.connect(self.openEEG)
+        self.eeg_menu = self.menuBar.addMenu('&File')
+        self.load_file = QtGui.QAction("&Load File", MainWindow)
+        self.load_file.triggered.connect(self.openEEG)
+        self.eeg_menu.addAction(self.load_file)
 
 
 
@@ -91,8 +93,12 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     # utility function for
+    def update_eeg_graph(self):
+        if(self.eeg is not None and self.eeg.shape[0] != 0):
+            self.updateData(self.eeg[0][-1] * self.eegScroll.value() / 1000);
+
     def lowestGreaterThan(self, arr, threshold):
-        print(arr.shape)
+        # print(arr.shape)
         low = 0
         high = len(arr)
         while low < high:
@@ -110,7 +116,7 @@ class Ui_MainWindow(object):
         return low
 
     def GreatestLowerThan(self, arr, threshold):
-        print(arr.shape)
+        # print(arr.shape)
         low = 0
         high = len(arr)
         while low < high:
@@ -127,8 +133,8 @@ class Ui_MainWindow(object):
                 high = low
         return high
 
-    def drawCurve(self, egg):
-        self.egg = egg;
+    def drawCurve(self, eeg):
+        self.eeg = eeg;
         self.time = 0
         start_time = self.time - 2000
         end_time = self.time + 2000
@@ -139,16 +145,16 @@ class Ui_MainWindow(object):
         self.view_plot = self.graphicsView.plot(self.x, self.y, pen=(0,1))
 
     def cal_content(self, start_time=-2000, end_time=2000):
-        low = self.lowestGreaterThan(self.egg[0, :], start_time)
-        high = self.GreatestLowerThan(self.egg[0, :], end_time)
+        low = self.lowestGreaterThan(self.eeg[0, :], start_time)
+        high = self.GreatestLowerThan(self.eeg[0, :], end_time)
         # limit the index in case out of array bound
-        low = min(self.egg.shape[1] - 1,max(0,low))
-        high = max(0,min(self.egg.shape[1] - 1, high))
-        print(start_time, end_time, self.egg[0, low], self.egg[0, high], low, high)
-        return self.egg[1,low:high + 1], self.egg[0,low:high + 1]
+        low = min(self.eeg.shape[1] - 1,max(0,low))
+        high = max(0,min(self.eeg.shape[1] - 1, high))
+        # print(start_time, end_time, self.eeg[0, low], self.eeg[0, high], low, high)
+        return self.eeg[1,low:high + 1], self.eeg[0,low:high + 1]
 
-    def updateData(self):
-        self.time = self.time + 100
+    def updateData(self, time):
+        self.time = time;
         start_time = self.time - 2000
         end_time = self.time + 2000
         yd, xd = self.cal_content(start_time=start_time,end_time=end_time)
@@ -161,30 +167,30 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
 
 
-    def openEEG(self, filename = None):
-        if filename is None:
-            filename = QtGui.QFileDialog.getOpenFileName(self, "Open File", user.home)
+    def openEEG(self, filename=None):
+        filename = QtGui.QFileDialog.getOpenFileName(self.centralWidget, "Open File for eeg", "./")
         if not filename:
             return
-        self.eeg = np.loadtxt(filename,delimiter=",")
+        self.drawCurve(np.loadtxt(str(filename),delimiter=",")[0:2,:])
 
     def SyncScroll(self):
         sliderValue = self.vlcslider.value()
         self.eegScroll.setValue(sliderValue)
+        self.update_eeg_graph()
 
     def updateScroll(self):
         self.eegScroll.setValue(self.vlcslider.value())
+        self.update_eeg_graph()
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     MainWindow = QtGui.QMainWindow()
     MainWindow.resize(1920, 1080)
-    #eeg = openEEG("AZZ1_v2_Carol_30sec.txt")
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    MainWindow.show()
     #ui.drawCurve(eeg)
     # ui.cal_content()
-    MainWindow.show()
     # t = QtCore.QTimer()
     # t.timeout.connect(ui.updateData)
     # t.start(100)
